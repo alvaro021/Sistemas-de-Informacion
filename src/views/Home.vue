db33ea31c26b9535c387b7c0464a4043c8abac37
Gilio Linfati <g.linfati02@ufromail.cl>
2021-12-13 16:38:03 -0300

    Se cargan imágenes para vistas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
           placeholder="Usuario"
           v-model="username"
         />
-        <label for="username">Username</label>
+        <label for="username">Usuario</label>
       </div>
       <div class="form-group">
         <input
@@ -21,7 +21,7 @@
           placeholder="Contraseña"
           v-model="password"
         />
-        <label for="Password">Password</label>
+        <label for="Password">Contraseña</label>
       </div>
       <div class="button-area">
         <button class="btn btn-primary pull-right" @click="login()">
@@ -39,11 +39,13 @@ export default {
   template: "#login-form",
   data() {
     return {
-      rememberMe: false,
+     // rememberMe: false,
       username: "",
       password: "",
     };
   },
+ 
+
   beforeMount() {
     let init = 1000;
     setTimeout(function () {
@@ -130,7 +132,6 @@ input:placeholder-shown + label {
   -webkit-transform: translateY(15px);
   transform: translateY(15px);
   color: rgb(211, 211, 211);
-
 }
 .h1 {
   color: #fff;
